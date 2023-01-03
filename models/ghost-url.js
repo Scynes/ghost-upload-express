60a2224b2327c203c374e479b2fd35f1174c4d1f
Scynes <dustin.greyfield@gmail.com>
2023-01-03 15:40:46 -0500

    ghost url schema (for unique shortened urls)

---
+++ models/ghost-url.js
@@ -0,0 +1,30 @@
+/**
+ * Imports the mongoose modules.
+ */
+const MONGOOSE = require('mongoose');
+
+/**
+ * Holds the Schema object reference from the mongoose module.
+ */
+const Schema = MONGOOSE.Schema;
+
+/**
+ * Database document model for URL's...
+ */
+const GHOST_URL_SCHEMA = new Schema({
+
+    image: {
+        originalURL: {
+            type: String,
+            required: true
+        },
+        uniqueURL: {
+            type: String,
+            required: true
+        }
+    }
+
+}, { timestamps: true, collection: 'urls'});
+
+// Exports the schema for use.
+module.exports = MONGOOSE.model('Ghost', GHOST_URL_SCHEMA);
